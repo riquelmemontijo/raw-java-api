aff1e858263c8f24d1323cc6c571273556b656ee
Riquelme Montijo <riquelme.montijo@pm.me>
2025-10-26 20:52:46 -0300

    config(server): cria servidos simples na porta 8080 e o contexto /produtos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,9 @@
-void main() {
-    IO.println("Hello World!");
+import com.sun.net.httpserver.HttpServer;
+
+void main() throws IOException {
+    var server = HttpServer.create(new InetSocketAddress(8080), 0);
+    server.createContext("/produtos");
+    server.setExecutor(null);
+    server.start();
+    System.out.println("Servidor rodando na porta 8080");
 }
